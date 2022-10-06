3580f40dd2225ee3f57d94c3e1469dbf6fffea1a
Lucky Ligovi <luckyligovi@gmail.com>
2022-10-06 16:00:43 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {useState, } from "react";
+import NavBar from './NavBar';
+import CartContainer from './CartContainer';
+
+function App() {
+  return (
+    <main>
+      <NavBar />
+      <CartContainer />
+    </main>
+  )
+}
+
+export default App;
