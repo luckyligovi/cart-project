76c1ff6752f6ea966796b51d90bcf4f8264d6c97
Lucky Ligovi <luckyligovi@gmail.com>
2022-10-09 21:44:50 +0300

    components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import { useGlobalContext } from './context'
-import NavBar from './NavBar'
+
+// components
+import Navbar from './NavBar'
 import CartContainer from './CartContainer'
+// items
 
 function App() {
+  const { loading } = useGlobalContext()
+  if (loading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
   return (
     <main>
-      <NavBar />
+      <Navbar />
       <CartContainer />
     </main>
   )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { AppProvider } from './context';
-ReactDOM.render(
-  <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
-  </React.StrictMode>,
+
+ReactDOM.render(<App />,
+
   document.getElementById('root')
 );
