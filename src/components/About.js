cfc7e34573400202acb8a025483450ae59eed689
Lucky Ligovi <luckyligovi@gmail.com>
2022-10-12 16:42:23 +0300

    changes

--- src/components/About.js
+++ src/components/About.js
@@ -9,19 +9,27 @@ function About() {
       <Accordion.Item eventKey="0"> 
         <Accordion.Header>Accordion Item #1</Accordion.Header>
         <Accordion.Body>
-        The Samsung Galaxy S7 is the best phone in this category. Buy while stocks last! Only 10 remaining. Visit out store now.
+        The Samsung Galaxy S7 is the best phone in this category. <br/>
+          <ul>
+            <li>Buy while stocks last! Only 10 remaining. Visit out store now.</li>
+            <li>Buy while stocks last! Only 10 remaining. Visit out store now.</li>
+            <li>Buy while stocks last! Only 10 remaining. Visit out store now.</li>
+            <li>Buy while stocks last! Only 10 remaining. Visit out store now.</li>
+          </ul>
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="1">
         <Accordion.Header>Accordion Item #2</Accordion.Header>
         <Accordion.Body>
-        The Google pixel is the best phone in this category. Buy while stocks last! Only 65 remaining. Visit out store now.
+        The Google pixel is the best phone in this category. <br/>
+        Buy while stocks last! Only 65 remaining. Visit out store now.
         </Accordion.Body>
       </Accordion.Item>
       <Accordion.Item eventKey="2">
         <Accordion.Header>Accordion Item #3</Accordion.Header>
         <Accordion.Body>
-        The Xiaomi Redmi Note 2 is the best phone in this category. Buy while stocks last! Only 21 remaining. Visit out store now.
+        The Xiaomi Redmi Note 2 is the best phone in this category. <br/>
+        Buy while stocks last! Only 21 remaining. Visit out store now.
  
         </Accordion.Body>
       </Accordion.Item>
